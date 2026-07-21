21a12e14082eed9f75adca4a51af70e0be485eeb
himanshugirase12 <girasehemu1212@gmail.com>
2026-07-21 15:47:15 +0530

    create model folder and post method by which data is stored in database

--- Day-7/src/app.js
+++ Day-7/src/app.js
@@ -1,5 +1,25 @@
 const express = require("express");
+const noteModel=require("./models/notes.model")
+
+
 
 const app = express();
 
-module.exports=app;
+app.use(express.json())
+
+app.post("/notes",async (req,res)=>{
+
+    const {title,description} =req.body
+
+    const note = await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message:"Notes created successfully",
+        note
+    })
+})
+
+
+module.exports=app; 
