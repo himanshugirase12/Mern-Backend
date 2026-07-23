dd5004e1b77637b136099c22857670f935d0e65a
himanshugirase12 <girasehemu1212@gmail.com>
2026-07-23 23:54:56 +0530

    previous revision is done

--- Day-8/Backend/src/config/database.js
+++ Day-8/Backend/src/config/database.js
@@ -1,5 +1,21 @@
 const mongoose= require("mongoose")
 
+const dotenv = require("dotenv");
+const dns = require("dns");
+
+dns.setServers([
+  "8.8.8.8",
+  "1.1.1.1"
+]);
+
+dotenv.config();
+
+
 function connectToDB(){
-    mongoose.connect(MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI)
+        .then(()=>{
+            console.log("connected to database...")
+        })
 }
+
+module.exports=connectToDB
