feb3523e54a2a109cf2e8e86a201feac4508ace5
himanshugirase12 <girasehemu1212@gmail.com>
2026-07-24 14:08:56 +0530

    all api methods are done.

--- Day-8/Backend/src/app.js
+++ Day-8/Backend/src/app.js
@@ -2,21 +2,52 @@ const express = require("express");
 const noteModel=require("./models/note.model")
 
 const app =  express();
-app.use(express.json())
+app.use(express.json());
 
 app.post('/notes',async (req,res)=>{
-
     const {title,description}=req.body
 
     const note = await noteModel.create({
         title,description
-    })
-
+    })  
     res.status(201).json({
         message:"Notes created successfully",
         note
     })
-
 })
+
+
+app.get('/notes' , async(req,res)=>{
+    const note=await noteModel.find()
+
+    res.status(200).json({
+        message:"Notes fetch successfully",
+        note
+    })
+})
+
+app.delete('/notes/:id', async (req,res)=>{
+
+    const id = req.params.id
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"note deleted successfully"
+    })
+})
+
+app.patch('/notes/:id', async (req,res)=>{
+    const id = req.params.id
+    const { description } = req.body
+
+    await noteModel.findByIdAndUpdate(id,{ description })
+
+    res.status(200).json({
+        message:"Note updated successfully."
+    })
+})
+
+
 
 module.exports=app
