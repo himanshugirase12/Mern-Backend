b0f50d84fd66d6040f694d100abf8196abff4abe
himanshugirase12 <girasehemu1212@gmail.com>
2026-08-05 16:08:51 +0530

    feat: implement user identification middleware and integrate it into post routes

--- Day-12/src/routes/post.routes.js
+++ Day-12/src/routes/post.routes.js
@@ -3,15 +3,16 @@ const postRouter =express.Router();
 const postController = require("../controllers/post.controller")
 const multer = require("multer"); 
 const upload = multer({ storage:multer.memoryStorage() })
+const identifyUser = require("../middlewares/auth.middleware")
 
-postRouter.post("/",upload.single("image"),postController.createPostController);
+postRouter.post("/",upload.single("image"),identifyUser,postController.createPostController);
 
 
-postRouter.get("/",postController.getPostController);
+postRouter.get("/",identifyUser,postController.getPostController);
 
 // Get /api/posts/details/:postId
 // return an detail about specific post with the id ,also check whether the post belongs to the user that the request come from
 
-postRouter.get("/details/:postId",postController.getPostDetailsController);
+postRouter.get("/details/:postId",identifyUser,postController.getPostDetailsController);
 
 module.exports=postRouter;  
