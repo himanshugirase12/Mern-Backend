587cc43ec34792a7c7b210c97755e7a866d00a33
himanshugirase12 <girasehemu1212@gmail.com>
2026-08-05 10:58:29 +0530

    feat: implement getPost and getPostDetails functionality with JWT validation

--- Day-12/src/routes/post.routes.js
+++ Day-12/src/routes/post.routes.js
@@ -6,4 +6,12 @@ const upload = multer({ storage:multer.memoryStorage() })
 
 postRouter.post("/",upload.single("image"),postController.createPostController);
 
+
+postRouter.get("/",postController.getPostController);
+
+// Get /api/posts/details/:postId
+// return an detail about specific post with the id ,also check whether the post belongs to the user that the request come from
+
+postRouter.get("/details/:postId",postController.getPostDetailsController);
+
 module.exports=postRouter;  
